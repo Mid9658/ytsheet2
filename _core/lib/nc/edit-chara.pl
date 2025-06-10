8785eff6a710a41f1e1b19d51a200d86f3e83e10
Mid9658 <54149117+Mid9658@users.noreply.github.com>
2025-06-10 20:03:18 +0900

    Use templates for NC edit and view

--- _core/lib/nc/edit-chara.pl
+++ _core/lib/nc/edit-chara.pl
@@ -1,20 +1,45 @@
 use strict;
 use utf8;
 use open ":utf8";
+use HTML::Template;
 
 our %pc;
 our $mode;
 
-my $title = ($mode eq 'edit') ? '編集' : '新規作成';
+my $titleName = ($mode eq 'edit') ? '編集' : '新規作成';
+
+my $tmpl = HTML::Template->new(
+  filename          => $::core_dir.'/skin/nc/edit-chara.html',
+  utf8              => 1,
+  path              => ['./', $::core_dir.'/skin/nc', $::core_dir.'/skin/_common', $::core_dir],
+  search_path_on_include => 1,
+  die_on_bad_params => 0,
+  case_sensitive    => 1,
+  global_vars       => 1,
+);
+
+$tmpl->param(
+  title        => $set::title,
+  titleName    => $titleName,
+  ver          => $main::ver,
+  coreDir      => $::core_dir,
+  id           => $pc{id},
+  characterName=> $pc{characterName},
+  playerName   => $pc{playerName},
+  aka          => $pc{aka},
+  age          => $pc{age},
+  gender       => $pc{gender},
+  enhanceArms  => $pc{enhanceArms},
+  enhanceMutate=> $pc{enhanceMutate},
+  enhanceModify=> $pc{enhanceModify},
+  actionPoint  => $pc{actionPoint},
+  madnessPoint => $pc{madnessPoint},
+  memory       => $pc{memory},
+  freeNote     => $pc{freeNote},
+  Menu         => [ { TEXT => '一覧へ', TYPE => 'href', VALUE => './', SIZE => 'small' } ],
+);
+
 print "Content-Type: text/html\n\n";
-print "<html><head><meta charset=\"UTF-8\"><title>$title - $set::title</title></head><body>";
-print "<h1>$title</h1>";
-print "<form method=\"post\" action=\"./\" enctype=\"multipart/form-data\">";
-print '<input type="hidden" name="mode" value="'.($mode eq 'edit'?'save':'make').'">';
-print '<input type="hidden" name="id" value="'.$pc{id}.'">' if $mode eq 'edit';
-print "<div>キャラクター名:<input name=\"characterName\" value=\"$pc{characterName}\"></div>";
-print "<div>プレイヤー名:<input name=\"playerName\" value=\"$pc{playerName}\"></div>";
-print '<div><input type="submit" value="保存"></div>';
-print "</form></body></html>";
+print $tmpl->output;
 
 1;
--- _core/lib/nc/view-chara.pl
+++ _core/lib/nc/view-chara.pl
@@ -1,13 +1,31 @@
 use strict;
 use utf8;
 use open ":utf8";
+use HTML::Template;
 
 our %pc = getSheetData();
 
+my $tmpl = HTML::Template->new(
+  filename          => $set::skin_sheet,
+  utf8              => 1,
+  path              => ['./', $::core_dir.'/skin/nc', $::core_dir.'/skin/_common', $::core_dir],
+  search_path_on_include => 1,
+  loop_context_vars => 1,
+  die_on_bad_params => 0,
+  case_sensitive    => 1,
+  global_vars       => 1,
+);
+
+$tmpl->param(
+  title        => $set::title,
+  ver          => $main::ver,
+  coreDir      => $::core_dir,
+  titleName    => 'キャラクターシート',
+  %pc,
+  Menu         => [ { TEXT => '編集', TYPE => 'href', VALUE => "./?mode=edit&id=$::in{id}", SIZE => 'small' } ],
+);
+
 print "Content-Type: text/html\n\n";
-print "<html><head><meta charset=\"UTF-8\"><title>$pc{characterName} - $set::title</title></head><body>";
-print "<h1>$pc{characterName}</h1>";
-print "<p>プレイヤー: $pc{playerName}</p>";
-print "</body></html>";
+print $tmpl->output;
 
 1;
