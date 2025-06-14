109ba89e6183c7aa866e330d1dd3f5cef67df2b6
Mid9658 <54149117+Mid9658@users.noreply.github.com>
2025-06-14 23:53:56 +0900

    Fix NC option select rendering

--- _core/lib/nc/subroutine-nc.pl
+++ _core/lib/nc/subroutine-nc.pl
@@ -1,6 +1,29 @@
 use strict;
 use utf8;
 
-# Placeholder for Nechronica specific routines
+# Nechronica 専用サブルーチン
+
+### select要素生成 --------------------------------------------------
+#   optionNc('name', @list)
+#   @list: 'value|<label>' ...
+sub optionNc {
+  my $name = shift;
+  my $text = '<option value=""></option>';
+  foreach my $i (@_) {
+    my $value = $i;
+    my $view;
+    if($value =~ s/^label=//){
+      $text .= '<optgroup label="'.$value.'">';
+      next;
+    }
+    elsif($value eq 'close_group') {
+      $text .= '</optgroup>';
+      next;
+    }
+    if($value =~ s/\|\<(.*?)\>$//){ $view = $1 } else { $view = $value }
+    $text .= '<option value="'.$value.'"'.($::pc{$name} eq $value ? ' selected':'').'>'.$view.'</option>';
+  }
+  return $text;
+}
 
 1;
